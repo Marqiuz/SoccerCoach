e0054b6044d2a9746476ebf4e7c16029995c80fb
Marqiuz <pr0t0we@abv.bg>
2022-04-04 14:07:53 +0300

    Starter register page in progress

---
+++ Data/SoccerCoach.Data/Migrations/20220404110559_DesignChanges.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SoccerCoach.Data.Migrations
+{
+    public partial class DesignChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Web/SoccerCoach.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(SoccerCoach.Web.Areas.Identity.IdentityHostingStartup))]
+
+namespace SoccerCoach.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
